5a315d75866d6c8e894163e1a69d51f7bc1724ce
Amaanchaudhary <chaudharyamaan982@gmail.com>
2024-01-29 22:50:56 +0530

    update

---
+++ bootstrap/src/App.js
@@ -0,0 +1,17 @@
+import AddTodo from "./components/AddTodo";
+import AppName from "./components/Heading";
+import TodoItem1 from "./components/TodoItem1";
+import TodoItem2 from "./components/TodoItem2";
+
+function App() {
+  return (
+    <center class="todo-container">
+      <AppName/>
+      <AddTodo/>
+      <TodoItem1/>
+      <TodoItem2/>
+    </center>
+  );
+}
+
+export default App;
